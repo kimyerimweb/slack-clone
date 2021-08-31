fa7c08621a70938f67d3ac14adc1e08ee05a59a5
kimyerimweb <kimyerimweb@gmail.com>
2021-08-31 16:03:20 +0900

    워크스페이스 뼈대 생성

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -3,8 +3,9 @@ import loadable from '@loadable/component';
 import { Switch, Route, Redirect } from 'react-router';
 import { Link } from 'react-router-dom';
 
-//const Login = loadable(() => import('@pages/Login'));
+const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
+const Channel = loadable(() => import('@pages/Channel'));
 
 const App = () => {
   return (
@@ -13,8 +14,9 @@ const App = () => {
       <Link to="/signup">회원가입 페이지</Link>
       <Switch>
         <Redirect exact path="/" to="/login" />
-        {/* <Route path="/login" component={Login} /> */}
+        <Route path="/login" component={LogIn} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/workspace/channel" component={Channel} />
       </Switch>
     </>
   );
