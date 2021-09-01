4def95f850cbb4d560738abbceea1bd43a968544
kimyerimweb <kimyerimweb@gmail.com>
2021-09-01 19:56:36 +0900

    layout내 컴포넌트 폴더로 분리

--- front/layouts/App/index.tsx
+++ front/layouts/App/index.tsx
@@ -6,17 +6,17 @@ import { Link } from 'react-router-dom';
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 const App = () => {
   return (
     <>
-      <Link to="/login">로그인 페이지</Link>
-      <Link to="/signup">회원가입 페이지</Link>
       <Switch>
         <Redirect exact path="/" to="/login" />
         <Route path="/login" component={LogIn} />
         <Route path="/signup" component={SignUp} />
         <Route path="/workspace/channel" component={Channel} />
+        <Route path="/workspace/dm" component={DirectMessage}></Route>
       </Switch>
     </>
   );
--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -3,6 +3,18 @@ import fetcher from '@utils/fetcher';
 import axios from 'axios';
 import useSWR from 'swr';
 import { Redirect } from 'react-router';
+import {
+  Header,
+  ProfileImg,
+  RightMenu,
+  WorkspaceWrapper,
+  Workspaces,
+  Channels,
+  WorkspaceName,
+  Chats,
+  MenuScroll,
+} from './styles';
+import gravatar from 'gravatar';
 
 const Workspace: FC = ({ children }) => {
   const { data, error, revalidate, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
@@ -18,7 +30,22 @@ const Workspace: FC = ({ children }) => {
 
   return (
     <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.nickname, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
       <button onClick={onLogout}>로그아웃</button>
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>menu scroll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
       {children}
     </div>
   );
