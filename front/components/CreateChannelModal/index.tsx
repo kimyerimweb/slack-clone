df4e148129d24e4e70f1ce576c0f7c83f539dac4
kimyerimweb <kimyerimweb@gmail.com>
2021-09-02 21:36:51 +0900

    채널 만드는 모달 컴포넌트 생성

---
+++ front/components/CreateChannelModal/index.tsx
@@ -0,0 +1,71 @@
+import Modal from '@components/Modal';
+import React, { useCallback, VFC } from 'react';
+import useInput from '@hooks/useInput';
+import { Button, Input, Label } from '@pages/SignUp/styles';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import { toast } from 'react-toastify';
+import fetcher from '@utils/fetcher';
+import useSWR from 'swr';
+import { IUser, IChannel } from '@typings/db';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  setShowCreateChannelModal: (flag: boolean) => void;
+}
+
+const CreateChannelModal: VFC<Props> = ({ show, onCloseModal, setShowCreateChannelModal }) => {
+  const [newChannel, onChangeNewChannel, setNewChannel] = useInput('');
+  const { workspace, channel } = useParams<{ workspace: string; channel: string }>();
+
+  const {
+    data: userData,
+    error,
+    revalidate,
+    mutate,
+  } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher);
+
+  const { data: channelDAta, revalidate: revalidateChannel } = useSWR<IChannel[]>(
+    userData ? `http://localhost:3095/api/workspaces/${workspace}/channels` : null,
+    fetcher,
+  );
+
+  const onCreateChannel = useCallback(
+    (e) => {
+      e.preventDefault();
+      axios
+        .post(
+          `http://localhost:3095/api/workspaces/${workspace}/channels`,
+          { name: newChannel },
+          {
+            withCredentials: true,
+          },
+        )
+        .then(() => {
+          revalidateChannel();
+          setShowCreateChannelModal(false);
+          setNewChannel('');
+        })
+        .catch((error) => {
+          console.dir(error);
+          toast.error(error.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newChannel],
+  );
+
+  return (
+    <Modal show={show} onCloseModal={onCloseModal}>
+      <form onSubmit={onCreateChannel}>
+        <Label id="channel-label">
+          <span>채널</span>
+          <Input id="channel" value={newChannel} onChange={onChangeNewChannel} />
+        </Label>
+        <Button type="submit">생성하기</Button>
+      </form>
+    </Modal>
+  );
+};
+
+export default CreateChannelModal;
