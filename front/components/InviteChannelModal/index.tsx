183ec4106e9193ebe281372a5e6f87e6292c9d92
kimyerimweb <kimyerimweb@gmail.com>
2021-09-04 21:57:39 +0900

    ✨ Implement channel invite modal component

---
+++ front/components/InviteChannelModal/index.tsx
@@ -0,0 +1,71 @@
+import Modal from '@components/Modal';
+import React, { useCallback, VFC } from 'react';
+import { Button, Input, Label } from '@pages/SignUp/styles';
+import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+import { toast } from 'react-toastify';
+import { IUser } from '@typings/db';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  setShowInviteChannelModal: (flag: boolean) => void;
+}
+
+const InviteChannelModal: VFC<Props> = ({ show, onCloseModal, setShowInviteChannelModal }) => {
+  const [newMember, onChangeNewMember, setNewMember] = useInput('');
+  const { workspace, channel } = useParams<{ workspace: string; channel: string }>();
+
+  const {
+    data: userData,
+    error,
+    revalidate,
+    mutate,
+  } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher);
+
+  const { data: memberData, revalidate: revalidateMember } = useSWR<IUser[]>(
+    userData ? `http://localhost:3095/api/workspaces/${workspace}/channels/${channel}/members` : null,
+    fetcher,
+  );
+
+  const onInviteMember = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!newMember || !newMember.trim()) {
+        return;
+      }
+
+      axios
+        .post(`http://localhost:3095/api/workspaces/${workspace}/channels/${channel}/members`, {
+          email: newMember,
+        })
+        .then(() => {
+          revalidateMember();
+          setShowInviteChannelModal(false);
+          setNewMember('');
+        })
+        .catch((error) => {
+          console.dir(error);
+          toast.error(error.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newMember],
+  );
+
+  return (
+    <Modal show={show} onCloseModal={onCloseModal}>
+      <form onSubmit={onInviteMember}>
+        <Label id="channel-label">
+          <span>이메일</span>
+          <Input id="channel" value={newMember} onChange={onChangeNewMember} />
+        </Label>
+        <Button type="submit">채널 초대하기</Button>
+      </form>
+    </Modal>
+  );
+};
+
+export default InviteChannelModal;
