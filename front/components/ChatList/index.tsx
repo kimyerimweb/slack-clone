ca89e0531b4db84a4ee8b37447599abaefaf5e8a
kimyerimweb <kimyerimweb@gmail.com>
2021-09-06 01:09:36 +0900

    💄  Add custom scrollbar

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,18 +1,24 @@
-import React, { VFC } from 'react';
+import React, { VFC, useRef, useCallback } from 'react';
 import { ChatZone } from './styles';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
   chatData?: IDM[];
 }
 
 const ChatList: VFC<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null);
+  const onScroll = useCallback(() => {}, []);
+
   return (
     <ChatZone>
-      {chatData?.map((chat) => {
-        return <Chat key={chat.id} data={chat} />;
-      })}
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat) => {
+          return <Chat key={chat.id} data={chat} />;
+        })}
+      </Scrollbars>
     </ChatZone>
   );
 };
