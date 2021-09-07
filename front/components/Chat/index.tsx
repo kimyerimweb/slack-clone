28becd6c7681dadbbbf7c566ded616d9e15627c5
kimyerimweb <kimyerimweb@gmail.com>
2021-09-07 18:56:06 +0900

    💄  Redirect DM page when click mention target Link

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -1,15 +1,40 @@
-import React, { VFC } from 'react';
+import React, { memo, VFC, useMemo } from 'react';
 import { IDM } from '@typings/db';
 import { ChatWrapper } from './style';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router';
 
 interface Props {
   data: IDM;
 }
 
 const Chat: VFC<Props> = ({ data }) => {
+  const { workspace } = useParams<{ workspace: string }>();
   const user = data.Sender;
+
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)\]\((\d+?)\)|\n/g,
+        decorator(match: string, index: number) {
+          const arr: string[] | null = match.match(/@\[(.+?)\]\((\d+?)\)/)!;
+          if (arr) {
+            return (
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+          return <br key={index} />;
+        },
+      }),
+    [data.content],
+  );
+
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -20,10 +45,10 @@ const Chat: VFC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span> {dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
 };
 
-export default Chat;
+export default memo(Chat);
