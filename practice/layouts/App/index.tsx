98ee923734791cba70998d5fd3914aab337d3447
kimyerimweb <kimyerimweb@gmail.com>
2021-09-10 13:38:56 +0900

    🎨  Add login, signup pages route

--- practice/layouts/App/index.tsx
+++ practice/layouts/App/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import loadable from '@loadable/component';
+import { Redirect, Route, Switch } from 'react-router';
+
+const Login = loadable(() => import('@pages/Login'));
+const Signup = loadable(() => import('@pages/Signup'));
 
 const App = () => {
-  return <div>안녕??</div>;
+  return (
+    <Switch>
+      <Redirect exact from="/" to="/login" />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+    </Switch>
+  );
 };
 
 export default App;
