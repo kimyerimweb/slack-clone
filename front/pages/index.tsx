1ad35f5342779f7b6682ac04641c846b00f9d756
kimyerimweb <kimyerimweb@gmail.com>
2022-04-16 18:41:27 +0900

    🚧 Prevent access to the home when not logged in

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,5 +1,28 @@
-import '../styles/home.module.scss'
+import "../styles/home.module.scss";
+import Head from "next/head";
+import fetcher from "../utils/fetcher";
+import useSWR from "swr";
+import { useEffect } from "react";
+import Router from "next/router";
 
-export default function Home() {
-    return <h1>안녕?</h1>
+function Home() {
+  const { data, error } = useSWR("http://localhost:3095/api/users", fetcher);
+
+  useEffect(() => {
+    if (!data) {
+      Router.replace("/login");
+    }
+  }, [data]);
+
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>안녕?</h1>
+    </>
+  );
 }
+
+export default Home;
