79c3abd477394206f54b53d2e498e4a09fc4f329
kimyerimweb <kimyerimweb@gmail.com>
2022-04-20 00:35:39 +0900

    🎨 Make style acceptable to Menu's child

--- front/components/Menu/index.tsx
+++ front/components/Menu/index.tsx
@@ -1,19 +1,20 @@
-import { ReactChild, useCallback } from "react";
+import { CSSProperties, ReactChild, useCallback } from "react";
 import styles from "./menu.module.scss";
 
 type MenuProp = {
   children: ReactChild;
+  style?: CSSProperties;
   handleCloseModal: (e: any) => void;
 };
 
-export default function Menu({ children, handleCloseModal }: MenuProp) {
+export default function Menu({ children, style, handleCloseModal }: MenuProp) {
   const stopPropagation = useCallback((e) => {
     e.stopPropagation();
   }, []);
 
   return (
     <div className={styles.createMenu} onClick={handleCloseModal}>
-      <div onClick={stopPropagation}>
+      <div onClick={stopPropagation} style={style}>
         <button className={styles.closeModalButton} onClick={handleCloseModal}>
           &times;
         </button>
